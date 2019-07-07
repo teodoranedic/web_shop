a28e510f09add4ce0c01ade49116ab47dc8c1237
MilicaPoparic <covekimastan@gmail.com>
2019-07-07 03:50:19 +0200

    ikonice dodate promenjen izgled nekih prozora

--- projekatSIMS/src/Controller/Main.java
+++ projekatSIMS/src/Controller/Main.java
@@ -5,13 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/View/StartWindow.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("GlobusApp");
+        primaryStage.getIcons().add(new Image("/icons/AppIcon.png"));
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
