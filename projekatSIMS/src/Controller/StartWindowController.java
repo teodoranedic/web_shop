08b264f1f4178055d6536b582af72fbd3632fe9a
MilicaPoparic <covekimastan@gmail.com>
2019-07-08 21:00:46 +0200

    filtriranja glavni prozor optimizacija

--- projekatSIMS/src/Controller/StartWindowController.java
+++ projekatSIMS/src/Controller/StartWindowController.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.print.PageOrientation;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -38,7 +39,7 @@ public class StartWindowController implements Initializable {
 
     @FXML
     public Label message = new Label("Nema rezultata pretrage.");
-    public TextField search = new TextField();
+    public TextField search = new TextField("");
     public Button searchButton = new Button();
     public ComboBox<String> male = new ComboBox<String>();
     public ComboBox<String> female = new ComboBox<String>();
@@ -98,24 +99,27 @@ public class StartWindowController implements Initializable {
 
     @FXML void comboChanged(ActionEvent event) throws IOException {
 
-        for (Kategorija k : kategorije) {
-            if (male.getValue()!= null && male.getValue().equals(k.getNaziv())) {
-                komboMuskarci = male.getValue();
-                komboZene = "";
-            }
-            if (female.getValue()!= null && female.getValue().equals(k.getNaziv())){
-                komboZene = female.getValue();
-                komboMuskarci = "";
-            }
+        if (male.getValue()!= null) {
+            komboMuskarci = male.getValue();
+            komboZene = "";
         }
-        for (Boja b : Main.boje.values()) {
-            if (color.getValue() != null && color.getValue().equals(b.getNaziv())) {
-                komboBoja = color.getValue();
-            }
+        if (female.getValue()!=null) {
+            komboZene = female.getValue();
+            komboMuskarci = "";
+        }
+        if (color.getValue()!=null) {
+            komboBoja = color.getValue();
         }
         if (price.getValue()!=null) {
             komboCena = price.getValue();
         }
+
+
+
+    }
+    @FXML
+    public void nadjiClicked(ActionEvent event) throws IOException {
+
         Parent registrovanjeParent = FXMLLoader.load(getClass().getResource("/View/StartWindow.fxml"));
         Scene registrovanjeScene = new Scene(registrovanjeParent);
 
@@ -124,8 +128,6 @@ public class StartWindowController implements Initializable {
 
         window.setScene(registrovanjeScene);
         window.show();
-
-
     }
 
     @FXML
@@ -142,171 +144,141 @@ public class StartWindowController implements Initializable {
 
 
     }
-    public void ispisProizvoda(HashMap<String,Proizvod> hm){
-        for (String s : hm.keySet()) {
-            Image image = new Image(s);
-            ImageView view = new ImageView(image);
 
-            // TODO: DODATI POSLE OVO U SVE FJE KOJR PRIKAZUJU SLIKE
-            view.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-                // na klik proizvoda setuje se trenutni proizvod i prelazi se na scenu za prikaz proizvoda
-                Main.trenutniProizvod = hm.get(s);
 
-                Parent proizvodParent = null;
-                try {
-                    proizvodParent = FXMLLoader.load(getClass().getResource("/View/Product.fxml"));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Scene proizvodScene = new Scene(proizvodParent);
-
-                //This line gets the Stage information
-                Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-
-                window.setScene(proizvodScene);
-                window.show();
-
-                event.consume();
-            });
-            root.getChildren().add(view);
-            root.setSpacing(10);
-            root.setPadding(new Insets(10));
-        }
-    };
-
-    public void ispisProizvodaP(HashMap<String, Proizvod> hm, String kombo) {
-        int uspesno = 0;
-        for (Proizvod p: hm.values()) {
-            if (p.getKategorija().getNaziv().equals(kombo)) {
-                uspesno = 1;
-                Image image = new Image(p.getSlika());
-                ImageView view = new ImageView(image);
-                root.getChildren().add(view);
-                root.setSpacing(10);
-                root.setPadding(new Insets(10));
-            }
-        }
-        if (uspesno==0){
+    public void ispisProizvoda(HashMap<String,Proizvod> hm, int reset){
+        if (hm.isEmpty()){
+            root.getChildren().clear();
             root.getChildren().add(message);
             root.setSpacing(10);
             root.setPadding(new Insets(10));
-        }
-    }
 
-    public int IspisBoja(HashMap<String,Proizvod> hm, int k) {
-        int uspesno = 0;
-        for (Proizvod p : hm.values()) {
-            for (Boja b : p.getBoja()) {
-                if (b.getNaziv().equals(komboBoja)){
-                    uspesno = 1;
-                    Image image = new Image(p.getSlika());
-                    ImageView view = new ImageView(image);
-                    if (k==1) {
-                        root.getChildren().clear();
-                    }
-                    root.getChildren().add(view);
-                    root.setSpacing(10);
-                    root.setPadding(new Insets(10));
-                }
-            }
         }
-        return uspesno;
-    }
-    HashMap<String,Proizvod> modifikovanaZene = new HashMap<String,Proizvod>();
-    public int IspisBojaZene(HashMap<String, Proizvod> hm) {
-        int uspesno = 0;
-        for (Proizvod p : hm.values()){ //svaki proizvod se proverava
-            if (p.getKategorija().getNaziv().equals(komboZene)){
-                modifikovanaZene.put(p.getSlika(), p);
-            }
-        }
-        if (!modifikovanaZene.isEmpty()) {
-            uspesno = IspisBoja(modifikovanaZene, 1);
-        }
-        return  uspesno;
-    }
-    HashMap<String,Proizvod> modifikovanaMuskarci = new HashMap<String,Proizvod>();
-    public int IspisBojaMuskarci(HashMap<String, Proizvod> hm) {
 
-        int uspesno = 0;
-        for (Proizvod p : hm.values()){ //svaki proizvod se proverava
-            if (p.getKategorija().getNaziv().equals(komboMuskarci)){
-                modifikovanaMuskarci.put(p.getSlika(), p);
-            }
-        }
-        if (!modifikovanaMuskarci.isEmpty()) {
-            uspesno = IspisBoja(modifikovanaMuskarci, 1);
-        }
-        return  uspesno;
-    }
-
-    public int IspisCena(HashMap<String,Proizvod>hm, int min, int max, int k) {
-        int uspesno = 0;
-        for (Proizvod p : hm.values()) {
-            if (min <= p.getStavkaCenovnika() && p.getStavkaCenovnika()<= max) {
-                Image image = new Image(p.getSlika());
+            for (String s : hm.keySet()) {
+                Image image = new Image(s);
                 ImageView view = new ImageView(image);
-                uspesno = 1;
-                if (k == 1) {
-                    root.getChildren().clear();
-                }
+
+                // TODO: DODATI POSLE OVO U SVE FJE KOJR PRIKAZUJU SLIKE
+                view.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+                    // na klik proizvoda setuje se trenutni proizvod i prelazi se na scenu za prikaz proizvoda
+                    Main.trenutniProizvod = hm.get(s);
+
+                    Parent proizvodParent = null;
+                    try {
+                        proizvodParent = FXMLLoader.load(getClass().getResource("/View/Product.fxml"));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Scene proizvodScene = new Scene(proizvodParent);
+
+                    //This line gets the Stage information
+                    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+                    window.setScene(proizvodScene);
+                    window.show();
+
+                    event.consume();
+                });
                 root.getChildren().add(view);
                 root.setSpacing(10);
                 root.setPadding(new Insets(10));
-            } else {}
-        }
-        return uspesno;
-    }
-    HashMap<String, Proizvod > zeneCeneModifikovana = new HashMap<String, Proizvod>();
-    public int IspisCenaZene(HashMap<String,Proizvod>hm, int min, int max, int k) {
-        int uspesno = 0;
-        for (Proizvod p : hm.values()){ //svaki proizvod se proverava
-            if (p.getKategorija().getNaziv().equals(komboZene)){
-                zeneCeneModifikovana.put(p.getSlika(), p);
+                if (reset!=0) {
+                    komboCena = "";
+                    komboBoja = "";
+                    komboMuskarci = "";
+                    komboZene = "";
+                }
             }
-        }
-        if (!zeneCeneModifikovana.isEmpty()) {
-            uspesno = IspisCena(zeneCeneModifikovana, min, max, k);
-        }
-        return  uspesno;
-    }
-    HashMap<String, Proizvod > muskarciCeneModifikovana = new HashMap<String, Proizvod>();
-    public int IspisCenaMuskarci(HashMap<String,Proizvod>hm, int min, int max, int k) {
-        int uspesno = 0;
-        for (Proizvod p : hm.values()){ //svaki proizvod se proverava
-            if (p.getKategorija().getNaziv().equals(komboMuskarci)){
-                muskarciCeneModifikovana.put(p.getSlika(), p);
-            }
-        }
-        if (!muskarciCeneModifikovana.isEmpty()) {
-            uspesno = IspisCena(muskarciCeneModifikovana, min, max, k);
-        }
-        return  uspesno;
-    }
+        scrollPane.setContent(root);
+        scrollPane.setPannable(true);
 
-    public void Pretraga(HashMap<String,Proizvod>hm) {
-        for (Proizvod p : hm.values()) {
+    };
+    HashMap<String, Proizvod> modifikovanaZene = new HashMap<String, Proizvod>();
+    HashMap<String, Proizvod> modifikovanaMuskarci = new HashMap<String, Proizvod>();
+    HashMap<String, Proizvod> modifikovanaNeutralno = new HashMap<String, Proizvod>();
+
+    public void izlistavanje() {
+        double min = 0.0;
+        double max = 0.0;
+        if (komboCena != "") {
+            min = Double.parseDouble(komboCena.substring(0, komboCena.indexOf("-")));
+            max = Double.parseDouble(komboCena.substring(komboCena.indexOf("-") + 1, komboCena.length()));
+        }
+        if (komboZene != "") {
+            MuskoZensko(proizvodiZenski,komboZene, modifikovanaZene, min, max);
+            ispisProizvoda(modifikovanaZene,1);
+        }
+        else if (komboMuskarci!="") {
+            MuskoZensko(proizvodiMuski, komboMuskarci, modifikovanaMuskarci, min, max);
+            ispisProizvoda(modifikovanaMuskarci,1);
+        }
+        else if (komboMuskarci.equals("") && (komboZene.equals(""))) {
+            Neutralno(proizvodiMuski, modifikovanaNeutralno, min, max);
+            Neutralno(proizvodiZenski, modifikovanaNeutralno, min, max);
+            ispisProizvoda(modifikovanaNeutralno,1);
+            }
+
+        }
+        public void Neutralno (HashMap<String, Proizvod> hm, HashMap<String, Proizvod> modifikovana, double min, double max){
+            for (Proizvod p: hm.values()) {
+                if (komboCena != "" && komboBoja != "") { //ako je odabrao i cenu i boju
+                    if (min <= p.getStavkaCenovnika() && p.getStavkaCenovnika() <= max) {
+                        for (Boja b : p.getBoja()) {
+                            if (komboBoja.equals(b.getNaziv())) {
+                                modifikovana.put(p.getSlika(), p);
+                            }
+                        }
+                    }
+                } else if (komboBoja != "" && komboCena == "") { //ako je odabrao samo cenu
+                    for (Boja b : p.getBoja()) {
+                        if (komboBoja.equals(b.getNaziv())) {
+                            modifikovana.put(p.getSlika(), p);
+                        }
+                    }
+                } else if (komboCena != "" && komboBoja == "") { //akoje odabrao samo boju
+                    if (min <= p.getStavkaCenovnika() && p.getStavkaCenovnika() <= max) {
+                        modifikovana.put(p.getSlika(), p);
+                    }
+                }
+
+            }
+            //ispisProizvoda(modifikovana,1);
+        }
+        public HashMap<String, Proizvod> modBojaCena= new HashMap<String, Proizvod>();
+        public void MuskoZensko(HashMap<String, Proizvod> hm, String kombo, HashMap<String, Proizvod> modifikacija, double min, double max){
+            for (Proizvod p : hm.values()) {
+                if (p.getKategorija().getNaziv().equals(kombo)) {
+                    modBojaCena.put(p.getSlika(), p);
+                }
+            }
+            Neutralno(modBojaCena, modifikacija, min, max);
+        }
+    public int Pretraga(HashMap<String,Proizvod>hm) {
+            int ret = 0;
+            for (Proizvod p : hm.values()) {
             if (trazi.equals(p.getNaziv().toUpperCase()) || trazi.equals(p.getNaziv().toLowerCase()) || trazi.equals(p.getNaziv()) ) {
+                ret = 1;
                 Image image = new Image(p.getSlika());
                 ImageView view = new ImageView(image);
+                root.getChildren().clear();
                 root.getChildren().add(view);
                 root.setSpacing(10);
                 root.setPadding(new Insets(10));
             }
             else {}
         }
-
-
+        return ret;
     }
 
 
-    @Override
+        @Override
     public void initialize(URL location, ResourceBundle resources) {
-        for (Kategorija k: kategorije) {
+        for (Kategorija k : kategorije) {
             maleList.add(k.getNaziv());
             femaleList.add(k.getNaziv());
         }
-        for (Boja b: Main.boje.values()) {
+        for (Boja b : Main.boje.values()) {
             colorList.add(b.getNaziv());
         }
 
@@ -317,88 +289,31 @@ public class StartWindowController implements Initializable {
         sort.setItems(sortList);
         price.setItems(priceList);
 
-        if (komboMuskarci.equals("") && komboZene.equals("") && komboSort.equals("") && komboBoja.equals("") && komboCena.equals("") && trazi.equals("")) {
-
-            ispisProizvoda(proizvodiZenski);
-            ispisProizvoda(proizvodiMuski);
-        }
-        if (komboMuskarci!="" ) { //ispis kada su odabrani muski proizvodi
-            ispisProizvodaP(proizvodiMuski, komboMuskarci);
+        if (komboZene.equals("") && komboMuskarci.equals("") && komboBoja.equals("") && komboCena.equals("") && search.getText().equals("")) {
+            ispisProizvoda(proizvodiMuski, 0);
+            ispisProizvoda(proizvodiZenski, 0);
         }
 
 
-        if (komboZene != "" ) { //ispis kada su odabrani zenski proizvodi
-            ispisProizvodaP(proizvodiZenski, komboZene);
+        if (komboZene!="" || komboMuskarci!="" || komboBoja!="" || komboCena!="") {
+            izlistavanje();
         }
 
-        if (komboBoja!="") { //ispis po boji
+        /*if (!trazi.equals("")){
             int uspesno = 0;
-            if (komboZene != "") {
-                uspesno += IspisBojaZene(proizvodiZenski);
-            }
-            else if (komboZene.equals("") && komboMuskarci.equals("")){
-                uspesno += IspisBoja(proizvodiMuski, 0); //samo muskarce proveri
-                uspesno += IspisBoja(proizvodiZenski, 0); //samo zene proveri
-            }
-            if (komboMuskarci!= "") {
-                uspesno += IspisBojaMuskarci(proizvodiMuski);
-            }
-            if (uspesno==0){
+            uspesno += Pretraga(proizvodiMuski);
+            uspesno += Pretraga(proizvodiZenski);
+            if (uspesno == 0) {
                 root.getChildren().clear();
                 root.getChildren().add(message);
                 root.setSpacing(10);
                 root.setPadding(new Insets(10));
-                komboMuskarci = "";
-                komboZene = "";
-                komboBoja = "";
             }
-            else {
-                komboMuskarci = "";
-                komboZene = "";
-                komboBoja = "";
-            }
-        }
+        }*/
 
-        if (komboCena != "") { //ispis po rasponu cene
-            int uspesno = 0;
-            int min = Integer.parseInt(komboCena.substring(0,komboCena.indexOf("-")));
-            int max = Integer.parseInt(komboCena.substring(komboCena.indexOf("-")+1, komboCena.length()));
-            if (komboZene != "") {
-                uspesno += IspisCenaZene(proizvodiZenski, min, max, 1);
-
-            }
-            else if (komboZene.equals("") && komboMuskarci.equals("")) {
-                uspesno += IspisCena(proizvodiMuski,min,max,0);
-                uspesno += IspisCena(proizvodiZenski,min,max,0);
-            }
-            else if (komboMuskarci!= "" ){
-                uspesno += IspisCenaMuskarci(proizvodiMuski, min, max, 1);
-
-            }
-            if (uspesno==0){
-                root.getChildren().clear();
-                root.getChildren().add(message);
-                root.setSpacing(10);
-                root.setPadding(new Insets(10));
-                komboMuskarci = "";
-                komboZene = "";
-                komboCena = "";
-            }
-            else {
-                komboMuskarci = "";
-                komboZene = "";
-                komboCena = "";
-            }
-        }
-
-        if (trazi != "") {
-            Pretraga(proizvodiMuski);
-            Pretraga(proizvodiZenski);
-            trazi = "";
-        }
-        scrollPane.setContent(root);
-        scrollPane.setPannable(true);
+    }
     }
 
-}
+
+
 
